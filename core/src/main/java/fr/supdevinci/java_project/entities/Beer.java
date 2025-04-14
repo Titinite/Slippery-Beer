9e45ac36bb68f93a6b3767e1709d087df7458464
Titinite <thibault.leray@supdevinci-edu.fr>
2025-04-14 14:44:30 +0200

    player screen

--- core/src/main/java/fr/supdevinci/java_project/entities/Beer.java
+++ core/src/main/java/fr/supdevinci/java_project/entities/Beer.java
@@ -1,4 +1,46 @@
 package fr.supdevinci.java_project.entities;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
 public class Beer {
+    private Vector2 position;
+    private Vector2 velocity;
+    private Texture texture;
+
+    private static final float GRAVITY = -20;
+    private static final float JUMP_VELOCITY = 350;
+    private static final float GROUND_Y = 100;
+
+    public Beer(float x, float y) {
+        position = new Vector2(x, y);
+        velocity = new Vector2(0, 0);
+        texture = new Texture("images/beer.png");
+    }
+
+    public void update(float deltaTime) {
+        velocity.add(0, GRAVITY);
+        velocity.scl(deltaTime);
+        position.add(0, velocity.y);
+
+        if (position.y < GROUND_Y) {
+            position.y = GROUND_Y;
+            velocity.y = 0;
+        }
+    }
+
+    public void jump() {
+        if (position.y == GROUND_Y) {
+            velocity.y = JUMP_VELOCITY;
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, position.x, position.y);
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
 }
--- core/src/main/java/fr/supdevinci/java_project/game/GameScreen.java
+++ core/src/main/java/fr/supdevinci/java_project/game/GameScreen.java
@@ -1,4 +1,50 @@
 package fr.supdevinci.java_project.game;
 
-public class GameScreen {
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import fr.supdevinci.java_project.Main;
+import fr.supdevinci.java_project.entities.Beer;
+
+public class GameScreen implements Screen {
+
+    private final Main game;
+    private Beer beer;
+
+    public GameScreen(Main game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        beer = new Beer(100, 100);
+    }
+
+    @Override
+    public void render(float delta) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            beer.jump();
+        }
+
+        beer.update(delta);
+
+        Gdx.gl.glClearColor(0.8f, 0.8f, 0.8f, 1); // fond gris clair
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        beer.render(game.batch);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+
+    @Override
+    public void dispose() {
+        beer.dispose();
+    }
 }
